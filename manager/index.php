8160d8c8fd25c30286ad83246f072a850c73d700
NH-X <1709597119@qq.com>
2023-06-04 13:21:33 +0800

    2023/06/04 13:21

--- manager/index.php
+++ manager/index.php
@@ -1,13 +1,13 @@
+
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title>
-
-        </title>
+        <title>Manager Homepage</title>
         <link rel="stylesheet" href="./css/style.css"/>
     </head>
     <body>
-        <div>
+        <div class="edit">
             <p>hello manager</p>
         </div>
     </body>
